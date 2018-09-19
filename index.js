5bc8b07f0bdaa024f3785e1818f28c64af119618
Victoria B <viktorija.bourassa@gmail.com>
2018-09-19 00:37:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,7 @@ function happyHolidayTo(holiday, name){
 return `Happy ${holiday}, ${name}!`;
 }
 function holidayCountdown(holiday, days){
+  var days = 20;
+  var holiday = "Mother's Day";
   return `It\'s ${days} days until  ${holiday}!`;
 }
